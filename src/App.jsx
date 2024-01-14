8e3ec89fb03f517e32f3658be75949622c0dacfd
Nisaru Abak <duck@MacBook-Pro-van-Nisaru.local>
2024-01-14 06:16:25 +0100

    Pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Pokemoncard from "./component/Pokemoncard.jsx";
 
 function App() {
+    const [pokeMon, setPokeMonData] = useState([]);
+    useEffect(() => {
+        async function fetchPokemon() {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20');
+                setPokeMonData(response.data.results);
+            } catch(e){
+                console.error(e);
+            }
+        }
+        fetchPokemon();
+    }, []);
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Gotta catch 'em all!</h1>
+            {pokeMon.length > 0 &&
+                pokeMon.map((pokemon, index) => {
+                    return <Pokemoncard key={index} name={pokemon.name} url={pokemon.url} />
+                })}
+        </>
+    );
 }
 
 export default App
---
+++ src/component/Pokemoncard.jsx
@@ -0,0 +1,8 @@
+
+function Pokemoncard({name, key, url}) {
+    return (
+        <div id={key}>{name}</div>
+    );
+}
+
+export default Pokemoncard;
